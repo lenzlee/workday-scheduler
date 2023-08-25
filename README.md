d8e470238296d4328774c08713ac4146415c72eb
Lenzlee Wheeler <lwheeler@luc.edu>
2023-08-24 22:43:11 -0500

    updated files

--- README.md
+++ README.md
@@ -5,17 +5,27 @@ Bootcamp Homework 5: Work Day Scheduler (Third-Party APIs)
 
 In this project, we were asked to modify a starter code to create a simple calendar application that allows a user to save events for each hour of a typical working day (9am–5pm). 
 
+When the page loads, the current date and time are displayed at the top of the page. The day calendar is also displayed on page load.
+
+Each row represents an hour of the day (.time-block). When the user enters an event for each row and clicks the save button, a "Saved!" message is displayed just below the date and time. The data entered is then recorded in the local storage. When the page loads, the information saved in the local storage is displayed on the page accordingly.
+
+Each row (.time-block) is color coded based on past (gray), present (red), or future (green) states. The states are determined by comparing the current hour to the numerical portion of the id assigned to each row (.time-block). If the *hourRow is less than the hourOfDay*, the class "past" is appended to the .time-block. If the *hourRow is equal to the hourOfDay*, it adds a class "present" and removes the class "past" to the .time-block. *Otherwise*, the classes "past" and "present" are removed and the class "future" is added to the .time-block. 
+
 ## Resources
 
 I used the following references for this assignment:
 
 **Day.js** (https://day.js.org/docs/en/display/format) to display the current date and time.
 
+I referred to our Javascript lessons and activities for this assignment. 
+
+I also used MDN web docs for reference when using the *localStorage* property, *split()* and *setTimeout()* methods, and a function's *this* keyword. 
+
 
 
 ## My Application Screenshot
 
-![alt text](./05-third-party-apis-homework-demo.gif)
+![alt text](./screenshot-workday-scheduler.png)
 
 ## Mockup Provided
 
--- script.js
+++ script.js
@@ -12,19 +12,17 @@ $(document).ready(function () {
   $('.saveBtn').on('click', function () {
 
   //get user input event, time of day, and save in localStorage
-
   var userInput = $(this).siblings('textarea').val();
   var currentTime = $(this).parent().attr('id');
 
   localStorage.setItem(currentTime, userInput);
 
   //display status Message when event and time data is saved to localStorage
-
   $('.statusMessage').addClass('show');
 
-  /* setTimeout(function () {
+ setTimeout(function () {
     $('.statusMessage').removeClass('show');
-  }, 5000); */
+  }, 5000);
 
 });
 
@@ -32,36 +30,37 @@ $(document).ready(function () {
 $('#hour-9 .description').val(localStorage.getItem('hour-9'));
 $('#hour-10 .description').val(localStorage.getItem('hour-10'));
 $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-$('#hour-noon .description').val(localStorage.getItem('hour-noon'));
-$('#hour-1pm .description').val(localStorage.getItem('hour-1pm'));
-$('#hour-2pm .description').val(localStorage.getItem('hour-2pm'));
-$('#hour-3pm .description').val(localStorage.getItem('hour-3pm'));
-$('#hour-4pm .description').val(localStorage.getItem('hour-4pm'));
-$('#hour-5pm .description').val(localStorage.getItem('hour-5pm'));
-
-});
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
 // function for color coding past, present, and future events
-$(function () {
+$(function updateTimeOfDay() {
+
+  var hourOfDay = dayjs().hour();
+
+  $('.time-block').each(function () {
+
+    var hourRow = parseInt($(this).attr('id').split('-')[1]);
+
+    if (hourRow < hourOfDay) {
+      $(this).addClass('past');
+    } 
+    else if (hourRow === hourOfDay) {
+      $(this).removeClass('past');
+      $(this).addClass('present');
+    } 
+    else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+  
+  }); 
 
 });
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+});
